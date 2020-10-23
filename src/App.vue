d91855b15d1cb89ede3049335395696083eff360
rizqi <rizqiramadhanrama@gmail.com>
2020-10-23 23:31:20 +0700

    menambahkan 4 menu terakhir

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
+  <div id="TechnologyFeature">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  </div>
+  <TechnologyFeature/>
+  <StayFocuses/>
+  <ContactSection/>
+  <InformationSection/>
+ 
+</div>
+  
+  
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+import TechnologyFeature from './components/1_technology.vue'
+import StayFocuses from './components/2_stay-focuses'
+import ContactSection from './components/3_contact.vue'
+import InformationSection from './components/4_information'
+
 
 export default {
-  name: 'App',
   components: {
-    // HelloWorld
+  TechnologyFeature,
+  StayFocuses,
+  ContactSection,
+  InformationSection
   }
 }
+
 </script>
 
 <style>
-#app {
+#TechnologyFeature {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+
+body {
+  margin:0;
 }
 </style>
